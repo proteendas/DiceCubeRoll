5da8b7e71e4358ca2f2701823b5bba8271627cce
proteendas <proteen.krdas.csbs24@heritageit.edu.in>
2023-05-26 21:41:08 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:dice_roll/gradient_container.dart';
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+            Color.fromARGB(255, 109, 83, 170),
+            Color.fromARGB(255, 218, 103, 57)),
+      ),
+    ),
+  );
+}
